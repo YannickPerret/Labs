a7624859436458fa0dfba9d99bcac1b2e937d350
tchoune <contact@yannickperret.com>
2020-07-16 16:21:11 +0200

    Add files via upload

---
+++ PooV2/app/public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../app/Autoloader.php';
+ \App\Autoloader::register();
+
+require 'Template/default.php';
+
+ob_start();
+
+
+if (isset($_GET['p']))
+{
+ $_GET['p'] == "home";
+ require '../pages/index.php';
+
+}
+else if($_GET['p'] === "slider")
+{
+    require '../public/slider.php';
+}
+else
+{
+    require '../public/index.php';
+}
+
+ob_get_clean();
